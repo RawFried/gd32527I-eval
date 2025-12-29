6c95378586e6535bb2b981e29499ba23cd594cf7
Raw_Fried <1664116766@qq.com>
2025-12-30 00:11:23 +0800

    - 添加flashdb测试

--- applications/main.c
+++ applications/main.c
@@ -14,23 +14,111 @@
 #include <rtdevice.h>
 #include <board.h>
 
+#include "flashdb.h"
+
 /* defined the LED1 pin: PE3 */
 #define LED1_PIN GET_PIN(E, 3)
 
+struct fdb_default_kv_node default_kv_table[] = {
+    {"key1", "value1", sizeof("value1")},
+    {"key2", "value2", sizeof("value2")},
+    {"key3", "value3", sizeof("value3")},
+};
+
+struct fdb_kvdb kvdb;
+struct fdb_tsdb tsdb;
+
+rt_mutex_t db_mutex;
+
+void lock(fdb_db_t db)
+{
+    rt_mutex_take(db_mutex, RT_WAITING_FOREVER);
+}
+
+void unlock(fdb_db_t db)
+{
+    rt_mutex_release(db_mutex);
+}
+
+static fdb_time_t counts = 0;
+fdb_time_t get_time(void)
+{
+    return ++counts;
+}
+
+extern void kvdb_basic_sample(fdb_kvdb_t kvdb);
+extern void kvdb_type_string_sample(fdb_kvdb_t kvdb);
+extern void kvdb_type_blob_sample(fdb_kvdb_t kvdb);
+extern void tsdb_sample(fdb_tsdb_t tsdb);
+
 int main(void)
 {
-    int count = 1;
+#ifdef FDB_USING_KVDB
+    { /* KVDB Sample */
+        struct fdb_default_kv default_kv;
 
-    /* set LED1 pin mode to output */
-    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+        /* create mutex */
+        db_mutex = rt_mutex_create("db_mutex", RT_IPC_FLAG_FIFO);
+        if (db_mutex == RT_NULL) {
+            return -1;
+        }
 
-    while (count++)
-    {
-        rt_pin_write(LED1_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED1_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+        default_kv.kvs = default_kv_table;
+        default_kv.num = sizeof(default_kv_table) / sizeof(default_kv_table[0]);
+        /* set the lock and unlock function if you want */
+        fdb_kvdb_control(&kvdb, FDB_KVDB_CTRL_SET_LOCK, lock);
+        fdb_kvdb_control(&kvdb, FDB_KVDB_CTRL_SET_UNLOCK, unlock);
+        /* Key-Value database initialization
+         *
+         *       &kvdb: database object
+         *       "env": database name
+         * "fdb_kvdb1": The flash partition name base on FAL. Please make sure it's in FAL partition table.
+         *              Please change to YOUR partition name.
+         * &default_kv: The default KV nodes. It will auto add to KVDB when first initialize successfully.
+         *        NULL: The user data if you need, now is empty.
+         */
+        fdb_err_t result = fdb_kvdb_init(&kvdb, "env", "fdb-kvdb", &default_kv, NULL);
+
+        if (result != FDB_NO_ERR) {
+            return -1;
+        }
+
+        /* run basic KV samples */
+        kvdb_basic_sample(&kvdb);
+        /* run string KV samples */
+        kvdb_type_string_sample(&kvdb);
+        /* run blob KV samples */
+        kvdb_type_blob_sample(&kvdb);
     }
+#endif /* FDB_USING_KVDB */
 
-    return RT_EOK;
+#ifdef FDB_USING_TSDB
+    { /* TSDB Sample */
+        /* set the lock and unlock function if you want */
+        fdb_tsdb_control(&tsdb, FDB_TSDB_CTRL_SET_LOCK, lock);
+        fdb_tsdb_control(&tsdb, FDB_TSDB_CTRL_SET_UNLOCK, unlock);
+        /* Time series database initialization
+         *
+         *       &tsdb: database object
+         *       "log": database name
+         * "fdb_tsdb1": The flash partition name base on FAL. Please make sure it's in FAL partition table.
+         *              Please change to YOUR partition name.
+         *    get_time: The get current timestamp function.
+         *         128: maximum length of each log
+         *        NULL: The user data if you need, now is empty.
+         */
+        fdb_err_t result = fdb_tsdb_init(&tsdb, "log", "fdb-tsdb", get_time, 128, NULL);
+        /* read last saved time for simulated timestamp */
+        fdb_tsdb_control(&tsdb, FDB_TSDB_CTRL_GET_LAST_TIME, &counts);
+
+        if (result != FDB_NO_ERR) {
+            return -1;
+        }
+
+        /* run TSDB sample */
+        tsdb_sample(&tsdb);
+    }
+#endif /* FDB_USING_TSDB */
+
+    return 0;
 }
